cbdb7f2b4726c05b9dd2cd97ecf250e057191d94
Harry Hogg <hhogg@spotify.com>
2022-03-21 11:21:43 +0000

    Added step for getting the adopter and contributor list

--- scripts/generateMetrics.ts
+++ scripts/generateMetrics.ts
@@ -1,4 +1,5 @@
 import { Buffer } from "buffer";
+import fetch from "node-fetch";
 import fs from "fs";
 import path from "path";
 import { Octokit, RestEndpointMethodTypes } from "@octokit/rest";
@@ -6,14 +7,25 @@ import minimist from "minimist";
 import { DateTime } from "luxon";
 import percentile from "percentile";
 import mean from "lodash.mean";
+import { Remarkable } from "remarkable";
+
+type GithubPull =
+  RestEndpointMethodTypes["pulls"]["list"]["response"]["data"][0];
+type GithubIssue =
+  RestEndpointMethodTypes["issues"]["list"]["response"]["data"][0];
+type GithubPullOrIssue = GithubPull | GithubIssue;
 
 const REPO_ORG = "backstage";
 const REPO_NAME = "tempo";
 const METRIC_FILENAME = "metrics.json";
+const ADOPTER_MD_URL =
+  "https://raw.githubusercontent.com/backstage/backstage/master/ADOPTERS.md";
+const PAST_DAYS_FOR_METRICS = 30;
 
 const argv = minimist(process.argv.slice(2));
-const isDryRun = "dryrun" in argv;
-const maxDaysToInclude = 30;
+const writeChanges = "writeChanges" in argv;
+const withMetrics = "withMetrics" in argv;
+const withAdopterList = "withAdopterList" in argv;
 
 if (!process.env.GITHUB_TOKEN) {
   console.error("GITHUB_TOKEN is not set. Please provide a Github token");
@@ -29,13 +41,41 @@ const iteratorRepos = octokit.paginate.iterator(octokit.rest.repos.listForOrg, {
   per_page: 100,
 });
 
-if (isDryRun) {
+if (!writeChanges) {
   console.log(
     "⚠️  Running in dryrun mode, no metrics will be commited back ⚠️"
   );
 }
 
-const getCurrentFileSha = async () => {
+const getAdopterList = async () => {
+  const adopters = new Set<string>();
+  const response = await fetch(ADOPTER_MD_URL);
+  const content = await response.text();
+  const tokens = new Remarkable().parse(content, {});
+
+  let isNextCell = false;
+
+  const isBlockToken = (
+    token: Remarkable.BlockContentToken
+  ): token is Remarkable.BlockContentToken =>
+    token.type === "inline" && Array.isArray(token.children);
+
+  for (const token of tokens) {
+    if (token.type === "tr_open") {
+      isNextCell = true;
+    } else if (isBlockToken(token) && isNextCell) {
+      if (token.content) {
+        adopters.add(token.content);
+      }
+
+      isNextCell = false;
+    }
+  }
+
+  return Array.from(adopters);
+};
+
+const getCurrentMetricFileSha = async () => {
   const contents = fs.readFileSync(
     path.resolve(__dirname, `../${METRIC_FILENAME}`),
     "utf8"
@@ -53,16 +93,29 @@ const getCurrentFileSha = async () => {
 };
 
 const main = async () => {
-  const namesOfAdopters: Set<string> = new Set();
+  /**
+   * Metric stores.
+   */
   const namesOfContributors: Set<string> = new Set();
   const secondsToClosePulls: number[] = [];
   const secondsToCloseIssues: number[] = [];
 
-  const getSecondsToClose = (
-    pullOrIssue:
-      | RestEndpointMethodTypes["pulls"]["list"]["response"]["data"][0]
-      | RestEndpointMethodTypes["issues"]["list"]["response"]["data"][0]
-  ) => {
+  /**
+   * Takes a pull request or issue and returns true/false
+   * if it's within the range for metrics.
+   */
+  const isPullOrIssueWithinMetricsRange = (pullOrIssue: GithubPullOrIssue) => {
+    return (
+      DateTime.fromISO(pullOrIssue.created_at).diff(DateTime.now(), "days")
+        .days < PAST_DAYS_FOR_METRICS
+    );
+  };
+
+  /**
+   * Takes a pull request or issue and returns the time from
+   * when it was opened to when it was closed.
+   */
+  const getSecondsToClose = (pullOrIssue: GithubPullOrIssue) => {
     if (pullOrIssue.closed_at) {
       const dateCreatedAt = DateTime.fromISO(pullOrIssue.created_at);
       const dateClosedAt = DateTime.fromISO(pullOrIssue.closed_at);
@@ -70,81 +123,87 @@ const main = async () => {
     }
   };
 
-  const getPullMetrics = (
-    pull: RestEndpointMethodTypes["pulls"]["list"]["response"]["data"][0]
-  ) => {
+  /**
+   * Takes a pull request, extract the metrics and stores them.
+   */
+  const getPullMetrics = (pull: GithubPull) => {
     const secondsToClose = getSecondsToClose(pull);
 
-    if (secondsToClose) secondsToClosePulls.push(secondsToClose);
-    if (pull.user?.name) namesOfContributors.add(pull.user.name);
+    if (secondsToClose && isPullOrIssueWithinMetricsRange(pull))
+      secondsToClosePulls.push(secondsToClose);
+    if (pull.user?.login) namesOfContributors.add(pull.user.login);
   };
 
-  const getIssueMetrics = (
-    issue: RestEndpointMethodTypes["issues"]["list"]["response"]["data"][0]
-  ) => {
+  /**
+   * Takes an pull request, extract the metrics and stores them.
+   */
+  const getIssueMetrics = (issue: GithubIssue) => {
     const secondsToClose = getSecondsToClose(issue);
 
-    if (secondsToClose) secondsToCloseIssues.push(secondsToClose);
-    if (issue.user?.name) namesOfContributors.add(issue.user.name);
+    if (secondsToClose && isPullOrIssueWithinMetricsRange(issue))
+      secondsToCloseIssues.push(secondsToClose);
+    if (issue.user?.login) namesOfContributors.add(issue.user.login);
   };
 
-  for await (const { data: repos } of iteratorRepos) {
-    for (const repo of repos) {
-      console.log("Processing repo: ", repo.name);
+  if (withMetrics) {
+    // For each repo in the backstage org
+    for await (const { data: repos } of iteratorRepos) {
+      for (const repo of repos) {
+        console.log("Processing repo: ", repo.name);
 
-      const iteratorPulls = octokit.paginate.iterator(octokit.rest.pulls.list, {
-        owner: REPO_ORG,
-        repo: repo.name,
-        per_page: 100,
-        sort: "created",
-        direction: "desc",
-        state: "closed",
-      });
+        const iteratorPulls = octokit.paginate.iterator(
+          octokit.rest.pulls.list,
+          {
+            owner: REPO_ORG,
+            repo: repo.name,
+            per_page: 100,
+            sort: "created",
+            direction: "desc",
+            state: "closed",
+          }
+        );
 
-      pullIterator: for await (const { data: pulls } of iteratorPulls) {
-        console.log("Processing pulls: ", pulls.length);
+        // For each pull request in the repo in the last N days
+        for await (const { data: pulls } of iteratorPulls) {
+          console.log("Processing pulls: ", pulls.length);
 
-        for (const pull of pulls) {
-          if (
-            DateTime.now().diff(DateTime.fromISO(pull.created_at), "days")
-              .days < maxDaysToInclude
-          ) {
+          for (const pull of pulls) {
             getPullMetrics(pull);
-          } else {
-            break pullIterator;
           }
         }
-      }
 
-      const iteratorIssues = octokit.paginate.iterator(
-        octokit.rest.issues.list,
-        {
-          owner: REPO_ORG,
-          repo: repo.name,
-          per_page: 100,
-          sort: "created",
-          direction: "desc",
-          state: "closed",
-        }
-      );
+        const iteratorIssues = octokit.paginate.iterator(
+          octokit.rest.issues.list,
+          {
+            owner: REPO_ORG,
+            repo: repo.name,
+            per_page: 100,
+            sort: "created",
+            direction: "desc",
+            state: "closed",
+          }
+        );
 
-      issueIterator: for await (const { data: issues } of iteratorIssues) {
-        console.log("Processing issues: ", issues.length);
+        // For each issue in the repo in the last N days
+        for await (const { data: issues } of iteratorIssues) {
+          console.log("Processing issues: ", issues.length);
 
-        for (const issue of issues) {
-          if (
-            DateTime.fromISO(issue.created_at).diff(DateTime.now(), "days")
-              .days < maxDaysToInclude
-          ) {
+          for (const issue of issues) {
             getIssueMetrics(issue);
-          } else {
-            break issueIterator;
           }
         }
       }
     }
   }
 
+  /**
+   * Fetch the latest adopters.
+   */
+  const adopterList = withAdopterList ? await getAdopterList() : [];
+
+  /**
+   * Calculate the final metrics from that in the stores.
+   */
   const p50SecondsToClosePulls = percentile(50, secondsToClosePulls);
   const p50SecondsToCloseIssues = percentile(50, secondsToCloseIssues);
   const meanSecondsToClosePulls = mean(secondsToClosePulls);
@@ -152,7 +211,7 @@ const main = async () => {
 
   const metrics = JSON.stringify(
     {
-      namesOfAdopters: Array.from(namesOfAdopters),
+      namesOfAdopters: adopterList,
       namesOfContributors: Array.from(namesOfContributors),
       p50SecondsToClosePulls,
       p50SecondsToCloseIssues,
@@ -163,10 +222,13 @@ const main = async () => {
     2
   );
 
-  console.log("Metrics:", metrics);
+  console.log(metrics);
 
-  if (!isDryRun) {
-    const sha = await getCurrentFileSha();
+  /**
+   * Commit the metrics back to the repo.
+   */
+  if (writeChanges) {
+    const sha = await getCurrentMetricFileSha();
 
     await octokit.rest.repos.createOrUpdateFileContents({
       owner: REPO_ORG,
